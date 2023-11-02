95accdfef83b752c12d0ee90a506f7744a27c7b1
yohanii <dygks924@gmail.com>
2023-11-03 00:03:06 +0900

    refactor: User 단순화

--- src/main/java/com/yohanii/lostandfound/domain/user/User.java
+++ src/main/java/com/yohanii/lostandfound/domain/user/User.java
@@ -1,13 +1,10 @@
 package com.yohanii.lostandfound.domain.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @NoArgsConstructor
 public class User {
 
@@ -18,15 +15,4 @@ public class User {
     private String nickName;
     private LocalDateTime createdTime;
     private LocalDateTime updatedTime;
-
-    @Builder
-    public User(String name, String loginId, String password) {
-        this.name = name;
-        this.loginId = loginId;
-        this.password = password;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
