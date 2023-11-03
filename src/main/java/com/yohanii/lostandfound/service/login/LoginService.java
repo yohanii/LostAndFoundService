e67ab5b5ffab0cd62323f6e367ee04a1fdee1796
yohanii <dygks924@gmail.com>
2023-11-03 12:30:14 +0900

    feat: LoginService 구현 및 UserMemoryRepository에 findByLoginId 추가

--- src/main/java/com/yohanii/lostandfound/service/login/LoginService.java
+++ src/main/java/com/yohanii/lostandfound/service/login/LoginService.java
@@ -1,7 +1,24 @@
 package com.yohanii.lostandfound.service.login;
 
+import com.yohanii.lostandfound.domain.user.User;
+import com.yohanii.lostandfound.domain.user.UserMemoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class LoginService {
+
+    private final UserMemoryRepository userRepository;
+
+    /**
+     * @return null, 로그인 실패 시
+     */
+    public User login(String loginId, String password) {
+        return userRepository.findByLoginId(loginId)
+                .filter(user -> user.getPassword().equals(password))
+                .orElse(null);
+    }
 }
