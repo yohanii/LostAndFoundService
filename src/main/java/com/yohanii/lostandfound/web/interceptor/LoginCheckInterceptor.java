8af1712cc10a20d3501207bddc3467da43af7ce9
yohanii <dygks924@gmail.com>
2023-11-03 16:59:10 +0900

    feat: 로그인 체크 인터셉터 추가

---
+++ src/main/java/com/yohanii/lostandfound/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,23 @@
+package com.yohanii.lostandfound.web.interceptor;
+
+import com.yohanii.lostandfound.domain.user.User;
+import com.yohanii.lostandfound.web.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession session = request.getSession();
+        if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
+            response.sendRedirect("/login?redirectURL=" + request.getRequestURI());
+            return false;
+        }
+
+        return true;
+    }
+}
