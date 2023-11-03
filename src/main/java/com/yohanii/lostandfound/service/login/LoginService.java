4715c478758a43861afa367547c241df85323ad5
yohanii <dygks924@gmail.com>
2023-11-03 12:16:53 +0900

    feat: LoginForm 보여주기까지 구현

---
+++ src/main/java/com/yohanii/lostandfound/service/login/LoginService.java
@@ -0,0 +1,7 @@
+package com.yohanii.lostandfound.service.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+}
---
+++ src/main/java/com/yohanii/lostandfound/web/login/LoginForm.java
@@ -0,0 +1,15 @@
+package com.yohanii.lostandfound.web.login;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+}
