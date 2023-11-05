0e5185d8393681cad4eeea69524c67527b13c09f
yohanii <dygks924@gmail.com>
2023-11-05 23:32:58 +0900

    feat: 게시글 보기 구현

--- src/main/java/com/yohanii/lostandfound/web/post/PostController.java
+++ src/main/java/com/yohanii/lostandfound/web/post/PostController.java
@@ -3,13 +3,17 @@ package com.yohanii.lostandfound.web.post;
 import com.yohanii.lostandfound.domain.post.Post;
 import com.yohanii.lostandfound.domain.post.PostMemoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/posts")
@@ -25,8 +29,11 @@ public class PostController {
         return "posts/posts";
     }
 
-    //@GetMapping("/{postId}")
-    public String post() {
-        return "";
+    @GetMapping("/{postId}")
+    public String post(@PathVariable Long postId, Model model) {
+        Post post = postRepository.findById(postId);
+
+        model.addAttribute("post", post);
+        return "posts/post";
     }
 }
