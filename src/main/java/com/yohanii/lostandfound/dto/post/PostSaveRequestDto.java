ee0d754884974ce5c681f6669bbed15bd16d218b
yohanii <dygks924@gmail.com>
2023-12-06 16:40:11 +0900

    refactor: PostRepository로 변경하면서 수정사항 반영

--- src/main/java/com/yohanii/lostandfound/dto/post/PostSaveRequestDto.java
+++ src/main/java/com/yohanii/lostandfound/dto/post/PostSaveRequestDto.java
@@ -2,6 +2,7 @@ package com.yohanii.lostandfound.dto.post;
 
 import com.yohanii.lostandfound.domain.post.Post;
 import com.yohanii.lostandfound.domain.post.PostType;
+import com.yohanii.lostandfound.domain.user.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -28,8 +29,9 @@ public class PostSaveRequestDto {
         this.type = type;
     }
 
-    public Post toEntity() {
+    public Post toEntity(User user) {
         return Post.builder()
+                .user(user)
                 .title(title)
                 .content(content)
                 .type(type)
