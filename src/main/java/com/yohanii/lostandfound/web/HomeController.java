11fc1aed614b4574752399a76b7c3b21d858f0e0
김요한 <kim-yohan@gim-yohan-ui-MacBookPro.local>
2024-01-09 17:20:25 +0900

    fix: home과 post에서 name 대신 nickName으로 볼 수 있게 수정, home에서 nickName 변경 반영 안되는 문제 해결

--- src/main/java/com/yohanii/lostandfound/web/HomeController.java
+++ src/main/java/com/yohanii/lostandfound/web/HomeController.java
@@ -1,7 +1,9 @@
 package com.yohanii.lostandfound.web;
 
 import com.yohanii.lostandfound.domain.user.User;
+import com.yohanii.lostandfound.domain.user.UserRepository;
 import com.yohanii.lostandfound.web.argumentresolver.Login;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserRepository userRepository;
 
     @GetMapping("/")
     public String home(@Login User loginUser, Model model) {
@@ -17,8 +22,10 @@ public class HomeController {
             return "home";
         }
 
+        User findUser = userRepository.find(loginUser.getId());
+
         log.info("loginUser={}", loginUser);
-        model.addAttribute("user", loginUser);
+        model.addAttribute("user", findUser);
         return "loginHome";
     }
 }
