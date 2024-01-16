26a5289d8598b049f2e86137ed5c775a7cdd3607
김요한 <kim-yohan@gim-yohan-ui-MacBookPro.local>
2024-01-16 16:03:40 +0900

    test: post search test 추가

--- src/test/java/com/yohanii/lostandfound/domain/post/PostRepositoryTest.java
+++ src/test/java/com/yohanii/lostandfound/domain/post/PostRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.yohanii.lostandfound.domain.post;
 
 import com.yohanii.lostandfound.domain.user.User;
+import com.yohanii.lostandfound.dto.post.PostSearchRequestDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -98,4 +102,57 @@ class PostRepositoryTest {
         assertThat(postsAfter).doesNotContain(post1);
     }
 
+    @Test
+    void findAllPostSearchRequestDto() {
+        Post post1 = Post.builder()
+                .content("!@#$%post1")
+                .type(PostType.FOUND)
+                .build();
+        Post post2 = Post.builder()
+                .content("!@#$%post2")
+                .type(PostType.FOUND)
+                .build();
+        Post post3 = Post.builder()
+                .content("!@#$%post3")
+                .type(PostType.LOST)
+                .build();
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+
+        List<Post> searchPosts1 = postRepository.findAll(new PostSearchRequestDto("!@#$", PostType.FOUND));
+        List<Post> searchPosts2 = postRepository.findAll(new PostSearchRequestDto("!@#$", PostType.LOST));
+        List<Post> searchPosts3 = postRepository.findAll(new PostSearchRequestDto("!@#$", null));
+
+        assertThat(searchPosts1.size()).isEqualTo(2);
+        assertThat(searchPosts2.size()).isEqualTo(1);
+        assertThat(searchPosts3.size()).isEqualTo(3);
+        assertThat(searchPosts1).contains(post1, post2);
+        assertThat(searchPosts2).contains(post3);
+        assertThat(searchPosts3).contains(post1, post2, post3);
+    }
+
+    @Test
+    void findAllPostSearchRequestDto_empty_content() {
+        Post post1 = Post.builder()
+                .content("!@#$%post1")
+                .type(PostType.FOUND)
+                .build();
+        Post post2 = Post.builder()
+                .content("!@#$%post2")
+                .type(PostType.FOUND)
+                .build();
+        Post post3 = Post.builder()
+                .content("!@#$%post3")
+                .type(PostType.LOST)
+                .build();
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+
+        List<Post> searchPosts = postRepository.findAll(new PostSearchRequestDto("", null));
+
+//        assertThat(searchPosts.size()).isEqualTo(3);
+        assertThat(searchPosts).contains(post1, post2, post3);
+    }
 }
