b412820012cf7fba148e61643f73573ccbec8020
yohan <dygks924@gmail.com>
2024-01-22 17:45:17 +0900

    fix: home에서 nickName 변경 반영 안되는 문제로 인해 코드 되돌리기

--- src/main/java/com/yohanii/lostandfound/web/HomeController.java
+++ src/main/java/com/yohanii/lostandfound/web/HomeController.java
@@ -1,5 +1,8 @@
 package com.yohanii.lostandfound.web;
 
+import com.yohanii.lostandfound.domain.user.User;
+import com.yohanii.lostandfound.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,13 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserRepository userRepository;
     @GetMapping("/")
     public String home(Model model) {
 
-        if (model.getAttribute("user") == null) {
+        User loginUser = (User) model.getAttribute("user");
+        if (loginUser == null) {
             return "home";
         }
+
+        User findUser = userRepository.find(loginUser.getId());
+        model.addAttribute("user", findUser);
+
         return "loginHome";
     }
 }
